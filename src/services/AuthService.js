a663bce97bbc63f295df675ed7b230908841fac6
koonlookhin <krit_tipnuan@cmu.ac.th>
2022-10-15 01:11:52 +0700

    4.8

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -18,6 +18,7 @@ export default {
       })
       .then((response) => {
         localStorage.setItem('token', response.data.token)
+        localStorage.setItem('user', JSON.stringify(response.data.user))
         return Promise.resolve(response.data)
       })
       .catch((error) => {
