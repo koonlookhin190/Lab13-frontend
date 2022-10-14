d6927246921cebaf6543821143d00c2a0e93a269
koonlookhin <krit_tipnuan@cmu.ac.th>
2022-10-15 00:17:34 +0700

    3.11

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { FontAwesomeIcon } from './plugins/font-awesome'
 
+import '@/services/AxiosInterceptorSetup.js'
 const requireComponent = require.context(
   './components',
   false,
