6d4445d4a71dfd665acba70d2776f1f33ab38683
koonlookhin <krit_tipnuan@cmu.ac.th>
2022-10-14 23:50:59 +0700

    3.7

--- src/views/LoginFormView.vue
+++ src/views/LoginFormView.vue
@@ -58,8 +58,8 @@ export default {
     }
   },
   methods: {
-    handleLogin(user){
-      AuthService.login(user).then((response)=>{
+    handleLogin(user) {
+      AuthService.login(user).then((response) => {
         console.log(response)
       })
     }
